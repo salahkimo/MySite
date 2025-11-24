d67f88b8270b51734ab2e844b59791b8509a2802
salahkimo <salahboudraakimodz@gmail.com>
2025-11-24 14:08:27 +0100

    Initial commit

---
+++ serveur.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("views"));
+
+// Charger les votes
+function loadVotes() {
+  return JSON.parse(fs.readFileSync("votes.json"));
+}
+
+// Sauvegarder les votes
+function saveVotes(votes) {
+  fs.writeFileSync("votes.json", JSON.stringify(votes));
+}
+
+// Page de vote
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// Soumission du vote
+app.post("/vote", (req, res) => {
+  const choice = req.body.choice;
+  const votes = loadVotes();
+
+  if (choice === "A") votes.A++;
+  if (choice === "B") votes.B++;
+
+  saveVotes(votes);
+  res.send("Merci pour votre vote !");
+});
+
+// Page admin résultats
+app.get("/results", (req, res) => {
+  const votes = loadVotes();
+  res.send(`
+    <h1>Résultats :</h1>
+    <p>A : ${votes.A}</p>
+    <p>B : ${votes.B}</p>
+  `);
+});
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
+
+
+app.get("/results-json", (req, res) => {
+  res.json(loadVotes());
+});
